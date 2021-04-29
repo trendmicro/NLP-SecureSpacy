8b11a4ca3a5ee0a21dde38884f4d49fa5f96cfcc
Joey Costoya <joey_costoya@trendmicro.com>
2021-04-30 00:09:51 +0800

    build a dictionary of intrusion sets that the spacy tokenizer should recognize

--- src/securespacy/tokenizer.py
+++ src/securespacy/tokenizer.py
@@ -1,4 +1,5 @@
 
+import os
 import re
 import spacy
 from spacy.tokenizer import Tokenizer
@@ -7,6 +8,25 @@ from .expressions import (
     detection_expr,
     cve_expr
 )
+
+
+def build_special_cases():
+
+    special_cases = {}
+    pwd = os.path.dirname(os.path.realpath(__file__))
+
+    with open(os.path.join(pwd, "data", "intrusion_set.txt"), "r") as fh:
+        for l in fh:
+            line = l.strip()
+
+            special_cases[line] = [
+                {
+                    "ORTH": line
+                }
+            ]
+
+    return special_cases
+
 
 #
 # define our custom Tokenizer function
@@ -19,6 +39,11 @@ def custom_tokenizer(nlp):
     #
     token_re = re.compile(f"{detection_expr}|{cve_expr}", re.VERBOSE | re.I | re.UNICODE)
 
+    #
+    # special cases dictionary
+    #
+    special_cases = build_special_cases()
+
     default_prefixes = nlp.Defaults.prefixes
     default_infixes = nlp.Defaults.infixes
     default_suffixes = nlp.Defaults.suffixes
@@ -29,6 +54,7 @@ def custom_tokenizer(nlp):
     infixes_re  = spacy.util.compile_infix_regex(default_infixes)
 
     return Tokenizer(nlp.vocab,
+                     rules=special_cases,
                      prefix_search=prefixes_re.search,
                      suffix_search=suffixes_re.search,
                      infix_finditer=infixes_re.finditer,
